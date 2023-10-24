f2c5b890c798d343d2caca23f16eee493aa8530b
IanCEHwang <cehwang1120@gmail.com>
2023-10-24 11:48:38 -0700

    Create 1435_Create_a_Session_Bar_Chart.sql

---
+++ SQL/Others/1435_Create_a_Session_Bar_Chart.sql
@@ -0,0 +1,15 @@
+select '[0-5>' as bin, sum(
+    case when duration >= 0 and duration < 300 then 1 else 0 end
+) as total from sessions
+union 
+select '[5-10>' as bin, sum(
+    case when duration >= 300 and duration < 600 then 1 else 0 end
+) as total from sessions
+union 
+select '[10-15>' as bin, sum(
+    case when duration >= 600 and duration < 900 then 1 else 0 end
+) as total from sessions
+union 
+select '15 or more' as bin, sum(
+    case when duration >= 900 then 1 else 0 end
+) as total from sessions
